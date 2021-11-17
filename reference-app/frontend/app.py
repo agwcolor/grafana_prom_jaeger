4a5bb8659bb0f5089c48f4447bdc8224ed84ffb8
Abbie Weisenbloom <Froggie@macbook-pro-3.lan>
2021-11-17 15:19:10 -0800

    First commit

---
+++ reference-app/frontend/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+from prometheus_flask_exporter import PrometheusMetrics
+
+# Frontend app
+app = Flask(__name__)
+
+metrics = PrometheusMetrics(app)
+
+# static information as metric
+metrics.info('app_info', 'Application info', version='1.0.3')
+
+@app.route('/')
+def homepage():
+    return render_template("main.html")
+
+
+if __name__ == "__main__":
+    app.run()
