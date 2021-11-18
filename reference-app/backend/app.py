0511e18a79c6036fd51a6db4d1830b3e384b30aa
Abbie Weisenbloom <Froggie@macbook-pro-3.lan>
2021-11-17 20:19:34 -0800

    Added jaeger tracing and service monitors

--- reference-app/backend/app.py
+++ reference-app/backend/app.py
@@ -1,14 +1,44 @@
 from flask import Flask, render_template, request, jsonify
 
 import pymongo
+import logging
 from flask_pymongo import PyMongo
 
 from prometheus_flask_exporter import PrometheusMetrics
 
+from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
+
+# Tracing
+from jaeger_client import Config
+from jaeger_client.metrics.prometheus import PrometheusMetricsFactory
+from opentelemetry import trace
+from opentelemetry.exporter import jaeger
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    ConsoleSpanExporter,
+    SimpleExportSpanProcessor,
+)
+
+trace.set_tracer_provider(TracerProvider())
+trace.get_tracer_provider().add_span_processor(
+    SimpleExportSpanProcessor(ConsoleSpanExporter())
+)
+
+
 # Backend app
 app = Flask(__name__)
 
+
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+
 metrics = PrometheusMetrics(app)
+# metrics = GunicornInternalPrometheusMetrics(app)
+
 # static information as metric
 metrics.info('app_info', 'Application info', version='1.0.3')
 
@@ -17,8 +47,32 @@ app.config['MONGO_URI'] = 'mongodb://example-mongodb-svc.default.svc.cluster.loc
 
 mongo = PyMongo(app)
 
+
+def init_tracer(service):
+    logging.getLogger('').handlers = []
+    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+
+    config = Config(
+        config={
+            'sampler': {
+                'type': 'const',
+                'param': 1,
+            },
+            'logging': True,
+        },
+        service_name=service,
+    )
+
+    # this call also sets opentracing.tracer
+    return config.initialize_tracer()
+
+tracer = init_tracer('backend-service')
+
+
 @app.route('/')
 def homepage():
+    with tracer.start_span('home-page') as span:
+        span.set_tag('first-tag', "I'm on the Home Page")
     return "Hello World"
 
 
