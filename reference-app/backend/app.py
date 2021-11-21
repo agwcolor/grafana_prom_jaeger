14bb51fb8ed3683fd2106a3f522a751ab72f3ea0
Abbie Weisenbloom <Froggie@macbook-pro-3.lan>
2021-11-21 01:05:25 -0800

    Working on tracing. Added 1 Jaeger trace

--- reference-app/backend/app.py
+++ reference-app/backend/app.py
@@ -4,52 +4,53 @@ import pymongo
 import logging
 from flask_pymongo import PyMongo
 
-from prometheus_flask_exporter import PrometheusMetrics
-
 # https://github.com/rycus86/prometheus_flask_exporter/blob/master/examples/gunicorn-internal/config.py
 from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
 
-def child_exit(server, worker):
-    GunicornInternalPrometheusMetrics.mark_process_dead_on_child_exit(worker.pid)
 
 # Tracing
 from jaeger_client import Config
-from jaeger_client.metrics.prometheus import PrometheusMetricsFactory
+
 from opentelemetry import trace
-from opentelemetry.exporter import jaeger
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
+
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import (
-    ConsoleSpanExporter,
-    SimpleExportSpanProcessor,
-)
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter
 
 # from prometheus_flask_exporter import PrometheusMetrics
-# Since we're using gunicorn, use this - https://github.com/rycus86/prometheus_flask_exporter/blob/master/examples/gunicorn-internal/requirements.txt
+# Since we're using gunicorn - https://github.com/rycus86/prometheus_flask_exporter/blob/master/examples/gunicorn-internal
 from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
 
 
-trace.set_tracer_provider(TracerProvider())
-trace.get_tracer_provider().add_span_processor(
-    SimpleExportSpanProcessor(ConsoleSpanExporter())
-)
+# Jaeger Tracing Config
+'''
+trace.set_tracer_provider(TracerProvider(
+    TracerProvider(
+        resource=Resource.create({SERVICE_NAME: "backend"})
+    )
+))
 
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(JaegerExporter())
+    )
+
+tracer = trace.get_tracer(__name__)
+'''
 
 # Backend app
 app = Flask(__name__)
 
-
-FlaskInstrumentor().instrument_app(app)
+FlaskInstrumentor().instrument_app(app, excluded_urls="metrics")
 RequestsInstrumentor().instrument()
 
 # metrics = PrometheusMetrics(app, group_by='endpoint')
 metrics = GunicornInternalPrometheusMetrics(app)
-
 # static information as metric
-metrics.info('backend_app_info', 'Backend App Prometheus Metrics', version='1.0.3')
+# metrics.info('backend_app_info', 'Backend App Prometheus Metrics', version='1.0.3')
 
 app.config['MONGO_DBNAME'] = 'example-mongodb'
 app.config['MONGO_URI'] = 'mongodb://example-mongodb-svc.default.svc.cluster.local:27017/example-mongodb'
@@ -57,6 +58,7 @@ app.config['MONGO_URI'] = 'mongodb://example-mongodb-svc.default.svc.cluster.loc
 mongo = PyMongo(app)
 
 
+# Another way to configure a tracer
 def init_tracer(service):
     logging.getLogger('').handlers = []
     logging.basicConfig(format='%(message)s', level=logging.DEBUG)
@@ -77,17 +79,17 @@ def init_tracer(service):
 
 tracer = init_tracer('backend')
 
-
 @app.route('/')
 def homepage():
-    with tracer.start_span('home-page') as span:
-        span.set_tag('first-tag', "I'm on the Home Page")
-    return "Hello World"
+    with tracer.start_active_span('home-page'):
+        answer = "I'm on the home page"
+    return jsonify(response=answer)
 
 
 @app.route('/api')
 def my_api():
-    answer = "something"
+    with tracer.start_span('my-api'):
+        answer = "something"
     return jsonify(response=answer)
 
 @app.route('/star', methods=['POST'])
